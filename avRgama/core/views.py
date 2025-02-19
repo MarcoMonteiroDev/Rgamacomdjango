ba57b42be25022ed4b352840b4b43f558dde5dde
Marco Monteiro <marcomont225@gmail.com>
2025-02-19 11:39:47 -0300

    dados_prontos_para_ir_para_template

--- avRgama/core/views.py
+++ avRgama/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "produtos": Produto.objects.all()
+    }
+
+    return render(request, "index.html", context)
