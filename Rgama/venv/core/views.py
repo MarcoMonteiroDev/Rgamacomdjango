9ca02e98ebf04647423aac24a224192579c98ceb
Marco Monteiro <marcomont225@gmail.com>
2025-04-05 23:04:50 -0300

    barra de pesquisa

--- Rgama/venv/core/views.py
+++ Rgama/venv/core/views.py
@@ -10,20 +10,24 @@ class IndexView(ListView):
     paginate_by = 30
 
     def get_queryset(self):
-        categoria = self.request.GET.get("categoria")
+        categoria = self.request.GET.get("categoria") # aqui ele pega o valor passado para a url pelo metodo request la no ancora quando passo ?categoria=value
+        busca = self.request.GET.get("busca")
         queryset = Produto.objects.all()
 
         if categoria:
-            queryset = queryset.filter(categoria__iexact=categoria)
+            queryset = queryset.filter(categoria__iexact=categoria) #categoria_iexact = case-insensitive exact match para evitar erro de case sensitive
+
+        if busca:
+            queryset = queryset.filter(nome__icontains=busca)
 
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context["produtos"] = self.object_list
+        context["produtos"] = self.object_list #traz a lista do query set
         context["promocoes"] = Promo.objects.all()
-        context["categorias"] = Produto.CAT_CHOICES
-        context["categoria_selecionada"] = self.request.GET.get("categoria","") #para usar no if para deixar a cat selecionada preta
+        context["categorias"] = Produto.CAT_CHOICES #para trazer as categorias para serem usadas no laço de repetição
+        context["categoria_selecionada"] = self.request.GET.get("categoria","") #para usar no if para deixar a cat selecionada preta retorna o valor associado a categoria categoria=valor se nao houver retorna vazio
         
         return context
 
@@ -31,3 +35,8 @@ class ProdutoView(DetailView):
     template_name = "produto.html"
     model = Produto
     context_object_name = 'produto'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProdutoView,self).get_context_data(**kwargs)
+        context["categorias"] = Produto.CAT_CHOICES #para trazer as categorias para serem usadas no laço de repetição
+        return context
